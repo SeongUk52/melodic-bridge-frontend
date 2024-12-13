cf4901bbbf864182c1dd8924afca9ac633eae805
SeongUk52 <rkfka7849@gmail.com>
2024-12-13 12:40:04 +0900

    feat: 인덱스 페이지로 라우팅

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,13 +2,16 @@
 
 import { useState, FormEvent } from "react";
 import axios from "axios";
+import { useRouter } from "next/navigation"; // next/navigation에서 useRouter를 임포트
 
 const Login: React.FC = () => {
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const router = useRouter(); // router 초기화
 
     const handleLogin = async (e: FormEvent) => {
         e.preventDefault();
+
         try {
             const response = await axios.post<{ token: string }>(
                 `${process.env.NEXT_PUBLIC_API_URL}/auth/login`, // 환경 변수 사용
@@ -17,6 +20,8 @@ const Login: React.FC = () => {
             // 로그인 성공 시 JWT 토큰 저장
             localStorage.setItem("token", response.data.token);
             alert("로그인 성공!");
+
+            await router.push("/"); // 홈으로 이동
         } catch (error) {
             console.error(error);
             alert("로그인 실패");
