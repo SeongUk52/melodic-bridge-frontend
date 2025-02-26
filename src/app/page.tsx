a5efd95feca0875f58703e52f0725e796e3390a3
SeongUk52 <rkfka7849@gmail.com>
2025-02-26 20:48:01 +0900

    feat: 로그인 상태일 경우 메인 페이지에서 로그인, 회원가입 버튼 숨김

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+"use client";
+
 import Link from 'next/link';
+import {useAuth} from "@/app/features/AuthContext";
 
 const HomePage: React.FC = () => {
+    const { isLoggedIn, logout } = useAuth();
     return (
         <div className="home-container">
             <h1>환영합니다! 서비스에 오신 것을 환영해요.</h1>
-            <p>서비스에 로그인하거나 회원가입하여 시작해보세요.</p>
-            <div>
-                <Link href="/login">로그인</Link>
-                <Link href="/signup">회원가입</Link>
-            </div>
+
+            {!isLoggedIn ? (
+                <>
+                    <p>서비스에 로그인하거나 회원가입하여 시작해보세요.</p>
+                    <div>
+                        <Link href="/login">로그인</Link>
+                        <Link href="/signup">회원가입</Link>
+                    </div>
+                </>
+                ) : (
+                <li><button onClick={logout}>로그아웃</button></li>
+            )}
         </div>
     );
 };
